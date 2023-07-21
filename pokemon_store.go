6f491df740b7c7b9c530bc943c0229921dafd4e5
Ignacio Saporiti <ignacio@saporiti.dev>
2023-07-21 17:13:05 +0200

    introduces scraper struct

--- pokemon_store.go
+++ pokemon_store.go
@@ -11,11 +11,51 @@ type pokemon struct {
 	url, image, name, price string
 }
 
+type pokemonScraper struct {
+	pokemonFound  []pokemon
+	pagesToScrape Pages
+	pagesVisited  Pages
+	iteration     int
+}
+
+func NewScraper() *pokemonScraper {
+	s := pokemonScraper{}
+	s.pagesVisited = append(s.pagesVisited, "1")
+	return &s
+}
+
+func (s *pokemonScraper) registerPage(page string) {
+	if s.pagesToScrape.contain(page) {
+		return
+	}
+	if s.pagesVisited.contain(page) {
+		return
+	}
+	s.pagesToScrape = append(s.pagesToScrape, page)
+}
+
+func (s *pokemonScraper) savePokemon(p pokemon) {
+	s.pokemonFound = append(s.pokemonFound, p)
+}
+
+func (s *pokemonScraper) nextPage() (url string) {
+	if len(s.pagesToScrape) == 0 {
+		return ""
+	}
+	pageToScrape := s.pagesToScrape[s.iteration]
+	url = getPageUrl(pageToScrape)
+	s.pagesToScrape = s.pagesToScrape[1:]
+	s.pagesVisited = append(s.pagesVisited, pageToScrape)
+	s.iteration++
+	return
+}
+
+func (s *pokemonScraper) pokemon() []pokemon {
+	return s.pokemonFound
+}
+
 func Scrape() {
-	var pokemonFound []pokemon
-	var pagesToScrape Pages
-	pagesVisited := Pages{"1"}
-	iteration := 0
+	scraper := NewScraper()
 	c := colly.NewCollector()
 
 	c.OnError(func(_ *colly.Response, err error) {
@@ -24,13 +64,7 @@ func Scrape() {
 
 	c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) {
 		page := e.Text
-		if pagesToScrape.contain(page) {
-			return
-		}
-		if pagesVisited.contain(page) {
-			return
-		}
-		pagesToScrape = append(pagesToScrape, page)
+		scraper.registerPage(page)
 	})
 
 	c.OnHTML("li.product", func(e *colly.HTMLElement) {
@@ -40,24 +74,17 @@ func Scrape() {
 			name:  e.ChildText("h2"),
 			price: e.ChildText("span.price"),
 		}
-		pokemonFound = append(pokemonFound, p)
+		scraper.savePokemon(p)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
-		if len(pagesToScrape) == 0 {
-			return
-		}
-		pageToScrape := pagesToScrape[iteration]
-		pagesToScrape = pagesToScrape[1:]
-		pagesVisited = append(pagesVisited, pageToScrape)
-		iteration++
-		url := getPageUrl(pageToScrape)
+		url := scraper.nextPage()
 		c.Visit(url)
 	})
 
 	firstPage := getPageUrl("1")
 	c.Visit(firstPage)
-	fmt.Print(len(pokemonFound))
+	fmt.Print(len(scraper.pokemon()))
 }
 
 func getPageUrl(page string) string {
