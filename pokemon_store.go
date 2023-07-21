ee0e3aa30424825d4cf433b1ef33de0720594e0a
Ignacio Saporiti <ignacio@saporiti.dev>
2023-07-21 16:55:32 +0200

    iterates pages

--- pokemon_store.go
+++ pokemon_store.go
@@ -1,6 +1,7 @@
 package pokemon_store
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gocolly/colly"
@@ -12,10 +13,24 @@ type pokemon struct {
 
 func Scrape() {
 	var pokemonFound []pokemon
+	var pagesToScrape Pages
+	pagesVisited := Pages{"1"}
+	iteration := 0
 	c := colly.NewCollector()
 
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong: ", err)
+	})
+
+	c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) {
+		page := e.Text
+		if pagesToScrape.contain(page) {
+			return
+		}
+		if pagesVisited.contain(page) {
+			return
+		}
+		pagesToScrape = append(pagesToScrape, page)
 	})
 
 	c.OnHTML("li.product", func(e *colly.HTMLElement) {
@@ -28,5 +43,34 @@ func Scrape() {
 		pokemonFound = append(pokemonFound, p)
 	})
 
-	c.Visit("https://scrapeme.live/shop/")
+	c.OnScraped(func(r *colly.Response) {
+		if len(pagesToScrape) == 0 {
+			return
+		}
+		pageToScrape := pagesToScrape[iteration]
+		pagesToScrape = pagesToScrape[1:]
+		pagesVisited = append(pagesVisited, pageToScrape)
+		iteration++
+		url := getPageUrl(pageToScrape)
+		c.Visit(url)
+	})
+
+	firstPage := getPageUrl("1")
+	c.Visit(firstPage)
+	fmt.Print(len(pokemonFound))
+}
+
+func getPageUrl(page string) string {
+	return fmt.Sprintf("https://scrapeme.live/shop/page/%s", page)
+}
+
+type Pages []string
+
+func (p Pages) contain(page string) bool {
+	for _, iterationPage := range p {
+		if iterationPage == page {
+			return true
+		}
+	}
+	return false
 }
